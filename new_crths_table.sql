c6f5080392f783fd0cd2efa6912347364ce65576
Alex Clark <amsclark@gmail.com>
2019-10-16 12:32:53 -0500

    initial commit

---
+++ new_crths_table.sql
@@ -0,0 +1,11 @@
+create table if not exists courthouses (
+  crths_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  crths_courtname varchar(50),
+  crths_countynum int(4),
+  crths_address1 varchar(50),
+  crths_address2 varchar(50),
+  crths_city varchar(50),
+  crths_state varchar(20),
+  crths_stt_abbrev varchar(2),
+  crths_zip varchar(10)
+);
