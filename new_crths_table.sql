5dcfab684064d12810ca62df0a378bc10a83c513
Alex Clark <amsclark@gmail.com>
2019-10-16 13:32:51 -0500

    county number not needed

--- new_crths_table.sql
+++ new_crths_table.sql
@@ -1,7 +1,6 @@
 create table if not exists courthouses (
   crths_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   crths_courtname varchar(50),
-  crths_countynum int(4),
   crths_address1 varchar(50),
   crths_address2 varchar(50),
   crths_city varchar(50),
